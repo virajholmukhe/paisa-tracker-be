1f5103729914aa8aca13902f83abbc61cbae7748
Viraj Holmukhe <virajholmukhe.com>
2023-08-05 16:28:34 +0530

    first commit

---
+++ src/main/java/com/pt/paisatracker/PaisaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pt.paisatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisaTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pt/paisatracker/PaisaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.paisatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
